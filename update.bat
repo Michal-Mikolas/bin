9028c8f14b60c44edcb0232b30a0ea9684c8a209
Michal Mikoláš <nanuqcz@gmail.com>
2024-08-01 22:22:44 +0200

    Create update.bat

---
+++ update.bat
@@ -0,0 +1,3 @@
+pip install --upgrade pip
+pip install --upgrade youtube-dl@git+https://github.com/ytdl-org/youtube-dl.git
+pip install --upgrade aider@git+https://github.com/paul-gauthier/aider.git
