68dfd677ad362d6016ac83ead8c314939e08992c
Michal Mikolas <nanuqcz@gmail.com>
2024-08-01 23:03:50 +0200

    Added installation and update script.

---
+++ install (AS ADMIN).bat
@@ -0,0 +1,4 @@
+cd /d "%~dp0"
+echo %PATH% > PATH.bak
+rem setx PATH2 "%PATH%;%USERPROFILE%\bin;"
+pause
--- update.bat
+++ update.bat
@@ -1,3 +1,3 @@
-pip install --upgrade pip
+python -m pip install --upgrade pip
 pip install --upgrade youtube-dl@git+https://github.com/ytdl-org/youtube-dl.git
-pip install --upgrade aider@git+https://github.com/paul-gauthier/aider.git
+pip install --upgrade aider-chat@git+https://github.com/paul-gauthier/aider.git
