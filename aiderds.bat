6fa658feae47d0fdaeece032bcaa899c262fbf6a
Michal Mikolas <nanuqcz@gmail.com>
2025-06-03 13:18:50 +0200

    Updated new models.

--- aiderds.bat
+++ aiderds.bat
@@ -1,2 +1,2 @@
 cd %CD%
-aider --model openrouter/deepseek/deepseek-chat --no-auto-commits --watch-files --model-settings-file %USERPROFILE%/bin/.aider.model.settings.yml --model-metadata-file %USERPROFILE%/bin/.aider.model.metadata.json --analytics --dark-mode --file readme.md %*
+aider --architect --model openrouter/deepseek/deepseek-chat-v3-0324:free --editor-model openrouter/deepseek/deepseek-chat-v3-0324:free --no-auto-commits --watch-files --model-settings-file %USERPROFILE%/bin/.aider.model.settings.yml --model-metadata-file %USERPROFILE%/bin/.aider.model.metadata.json --analytics --dark-mode --file readme.md %*
--- aiderr1.bat
+++ aiderr1.bat
@@ -1,2 +1,2 @@
 cd %CD%
-aider --architect --model openrouter/deepseek/deepseek-r1 --editor-model openrouter/deepseek/deepseek-chat --no-auto-commits --watch-files --model-settings-file %USERPROFILE%/bin/.aider.model.settings.yml --model-metadata-file %USERPROFILE%/bin/.aider.model.metadata.json --analytics --dark-mode --file readme.md %*
+aider --model openrouter/deepseek/deepseek-r1-0528:free --no-auto-commits --watch-files --model-settings-file %USERPROFILE%/bin/.aider.model.settings.yml --model-metadata-file %USERPROFILE%/bin/.aider.model.metadata.json --analytics --dark-mode --file readme.md %*
